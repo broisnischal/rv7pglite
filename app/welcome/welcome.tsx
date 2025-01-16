57abd5b72f89c45753a95e8210579aeeb63e7c7a
broisnischal <neeswebservice@gmail.com>
2025-01-16 20:41:59 +0545

    feat:publish

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,6 +4,10 @@ export function Welcome() {
     <main className="pt-16 p-4 container mx-auto">
       <h1 className="text-2xl">Pglite & Electric Sql with drizzle</h1>
 
+      <h2>
+        Testing rolling update
+      </h2>
+
       <p className="mt-10 text-gray-400">
         A contact application demonstrating local first database, Pglite and Electric Sql with drizzle and integrating in react router v7.
       </p>
