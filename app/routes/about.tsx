34ff45f8d35c0f989c6d11a9c6d74b07a1c0acb5
broisnischal <neeswebservice@gmail.com>
2025-01-16 22:39:21 +0545

    feat:pub

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,4 +1,32 @@
+import { useEffect, useRef } from "react";
+
 export default function About() {
+
+  const worker = useRef<Worker>(null);
+
+
+  useEffect(() => {
+
+    if (!worker.current) {
+      // Create the worker if it does not yet exist.
+      worker.current = new Worker(new URL('./worker.js', import.meta.url), {
+        type: 'module'
+      });
+    }
+
+    // Create a callback function for messages from the worker thread.
+    const onMessageReceived = (e) => {
+      // TODO: Will fill in later
+    };
+
+    // Attach the callback function as an event listener.
+    worker.current.addEventListener('message', onMessageReceived);
+    // Define a cleanup function for when the component is unmounted.
+    return () => worker.current?.removeEventListener('message', onMessageReceived);
+  }, []);
+
+
+
   return (
     <div>
       <h1>About</h1>
