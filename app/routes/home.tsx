4e8a8ea2a7d56a59123d2cedf982fcd224b26c2a
broisnischal <info@nischal-dahal.com.np>
2025-01-17 09:13:35 +0545

    feat:publish-deoly

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,25 +5,22 @@ import { Form, Link, useFetcher } from "react-router";
 import { users } from "~/database/.client/schema";
 import { useRef } from "react";
 
-export function meta({ }: Route.MetaArgs) {
+export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
-export async function clientLoader({ }: Route.ClientLoaderArgs) {
+export async function clientLoader({}: Route.ClientLoaderArgs) {
   const contacts = await db.query.users.findMany();
 
-  return { contacts }
+  return { contacts };
 }
 
 export default function Home({ loaderData, actionData }: Route.ComponentProps) {
-
-
   let fetcher = useFetcher();
   let busy = fetcher.state !== "idle";
-
 
   const formRef = useRef<HTMLFormElement>(null);
 
@@ -32,20 +29,38 @@ export default function Home({ loaderData, actionData }: Route.ComponentProps) {
       <h1 className="text-2xl">Pglite & Electric Sql with drizzle</h1>
 
       <p className="mt-10 text-gray-500">
-        A contact application demonstrating local first database, Pglite and Electric Sql with drizzle and integrating in react router v7.
+        A contact application demonstrating local first database, Pglite and
+        Electric Sql with drizzle and integrating in react router v7. checkout
+        the github{" "}
+        <Link
+          className="underline text-black"
+          to={"https://github.com/broisnischal/rv7pglite"}
+        >
+          repo
+        </Link>{" "}
+        🚀, and soon will be content related to this.
       </p>
 
       <div className="flex my-5 align-top gap-5">
         <div className="w-1/3">
-          <fetcher.Form ref={formRef} method="post" className="flex flex-col  gap-2 [&>input]:p-2 [&>input]:border w-full"  >
+          <fetcher.Form
+            ref={formRef}
+            method="post"
+            className="flex flex-col  gap-2 [&>input]:p-2 [&>input]:border w-full"
+          >
             <input type="text" name="firstname" placeholder="First Name" />
             <input type="text" name="lastname" placeholder="Last Name" />
             <input type="text" name="email" placeholder="Email address" />
             <input type="text" name="phone" placeholder="Phone number" />
-            <button type="submit" disabled={busy} onClick={() => {
-              fetcher.submit(formRef.current);
-              formRef.current?.reset();
-            }} className="bg-black p-2 text-white">
+            <button
+              type="submit"
+              disabled={busy}
+              onClick={() => {
+                fetcher.submit(formRef.current);
+                formRef.current?.reset();
+              }}
+              className="bg-black p-2 text-white"
+            >
               {busy ? "Adding..." : "Add Contact"}
             </button>
           </fetcher.Form>
@@ -55,24 +70,27 @@ export default function Home({ loaderData, actionData }: Route.ComponentProps) {
         </div>
 
         <div className="flex gap-4 flex-wrap">
-          {loaderData.contacts.length <= 0 ? (<div className="py-10">
-            <h1>No Contacts in your phonebook!</h1>
-          </div>) : loaderData.contacts.map((item) => (
-            <div className="py-4 px-6 w-fit h-min border rounded-md">
-              <h2>{item.firstName} {item.lastName}</h2>
-              <h1 className="text-gray-500">{item.email}</h1>
-              <Link to={`tel:${item.phone}`}>{item.phone}</Link>
-              {/* <Link to={`/contact/${item.id}`}>View User</Link> */}
+          {loaderData.contacts.length <= 0 ? (
+            <div className="self-start">
+              <h1 className="">No Contacts in your phonebook!</h1>
             </div>
-          ))}
+          ) : (
+            loaderData.contacts.map((item) => (
+              <div className="py-4 px-6 w-fit h-min border rounded-md">
+                <h2>
+                  {item.firstName} {item.lastName}
+                </h2>
+                <h1 className="text-gray-500">{item.email}</h1>
+                <Link to={`tel:${item.phone}`}>{item.phone}</Link>
+                {/* <Link to={`/contact/${item.id}`}>View User</Link> */}
+              </div>
+            ))
+          )}
         </div>
       </div>
-
-
     </main>
   );
 }
-
 
 export async function clientAction({ request }: Route.ClientActionArgs) {
   const formData = await request.formData();
